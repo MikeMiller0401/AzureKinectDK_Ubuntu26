f87c452a9420cc3c93051d6815d3c1b45988207a
MikeMiller0401 <lingu11436@gmail.com>
2026-06-29 10:51:42 +0800

    update save camera code

--- save_color&depth.py
+++ save_color&depth.py
@@ -3,46 +3,16 @@ import numpy as np
 import cv2
 import os
 from tqdm import tqdm
+from pyk4a import PyK4APlayback
 
-# config = o3d.io.read_azure_kinect_mkv_metadata("/home/tcy/PycharmProjects/AzureKinectDK_Ubuntu26/video/test003.mkv")
-# print(config["device_metadata"]["serial_number"])
-
-reader = o3d.io.AzureKinectMKVReader()
-# metadate = reader.get_metadata()
-video_name = "test004"
-video_path = os.path.join("video",video_name + ".mkv")
-output_path = os.path.join("output",video_name)
-
-success = reader.open(video_path)
-
-print("open:", success)
-
-
-if not success:
-    raise RuntimeError("Failed to open mkv file")
-
-color_path = os.path.join(output_path, "color")
-depth_path = os.path.join(output_path,"depth")
-os.makedirs(color_path, exist_ok=True)
-os.makedirs(depth_path, exist_ok=True)
-
-pbar = tqdm(desc="Decoding MKV frames", unit="frame")
-
-i = 0
-while not reader.is_eof():
-    rgbd = reader.next_frame()
-    if rgbd is None:
-        continue
-
-    if reader.is_eof():
-        break
-
-    # RGB
-    color = np.asarray(rgbd.color)
+def save_color(capture, color_path):
+    color = np.asarray(capture.color)
     color = cv2.cvtColor(color, cv2.COLOR_RGB2BGR)
     cv2.imwrite(f"{color_path}/{i:06d}.png", color)
 
-    depth = np.asarray(rgbd.depth)
+
+def save_depth(capture, depth_path):
+    depth = np.asarray(capture.depth)
     # 1. 先保存原始深度数据（这个数据本身是稳定的，单位是毫米 mm）
     np.save(os.path.join(depth_path, f"{i:06d}.npy"), depth)
 
@@ -62,10 +32,58 @@ while not reader.is_eof():
     # 5. 生成伪彩色并保存
     depth_heatmap = cv2.applyColorMap(depth_vis, cv2.COLORMAP_JET)
     cv2.imwrite(f"{depth_path}/{i:06d}.png", depth_heatmap)
-    i += 1
-    pbar.update(1)   # ✅ 更新进度条
 
-pbar.close()
-reader.close()
+def save_camera(video_path, save_path):
+    playback = PyK4APlayback(video_path)
+    playback.open()
 
-print("Done:", i, "frames")
+    K_rgb = playback.calibration.get_camera_matrix(0)
+    K_depth = playback.calibration.get_camera_matrix(1)
+    
+    np.savetxt(os.path.join(save_path, "K_rgb.txt"), K_rgb, fmt="%.8f")
+    np.savetxt(os.path.join(save_path, "K_depth.txt"), K_depth, fmt="%.8f")
+
+    playback.close()
+
+    print("Saved K_rgb.txt and K_depth.txt")
+  
+if __name__ == "__main__":
+    # config = o3d.io.read_azure_kinect_mkv_metadata("/home/tcy/PycharmProjects/AzureKinectDK_Ubuntu26/video/test003.mkv")
+    # print(config["device_metadata"]["serial_number"])
+
+    reader = o3d.io.AzureKinectMKVReader()
+    # metadate = reader.get_metadata()
+    video_name = "test004"
+    video_path = os.path.join("video", video_name + ".mkv")
+    output_path = os.path.join("output", video_name)
+    success = reader.open(video_path)
+    print("open:", success)
+
+    if not success:
+        raise RuntimeError("Failed to open mkv file")
+
+    color_path = os.path.join(output_path, "color")
+    depth_path = os.path.join(output_path,"depth")
+    os.makedirs(color_path, exist_ok=True)
+    os.makedirs(depth_path, exist_ok=True)
+
+    pbar = tqdm(desc="Decoding MKV frames", unit="frame")
+
+    i = 0
+    while not reader.is_eof():
+        rgbd = reader.next_frame()
+        if rgbd is None:
+            continue
+
+        if reader.is_eof():
+            break
+        save_color(rgbd, color_path)
+        save_depth(rgbd, depth_path)
+
+        i += 1
+        pbar.update(1)   # ✅ 更新进度条
+
+    pbar.close()
+    reader.close()
+    save_camera(video_path, output_path)
+    print("Done:", i, "frames")
--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from pyk4a import PyK4APlayback
 import os
 import numpy as np
-import cv2
 
 path = "video"
 mkv_path = os.path.join(path, "test004.mkv")
@@ -13,35 +12,4 @@ print(f"K_rgb: {K_rgb}")
 K_depth = playback.calibration.get_camera_matrix(1)
 print(f"K_depth: {K_depth}")
 
-rgb_dir = os.path.join(path, "rgb")
-depth_dir = os.path.join(path, "depth")
-os.makedirs(rgb_dir, exist_ok=True)
-os.makedirs(depth_dir, exist_ok=True)
-
-i = 0
-
-try:
-    while True:
-        capture = playback.get_next_capture()
-        color = capture.color    
-        if color is not None:
-            print(color.shape)
-            color = cv2.cvtColor(color, cv2.COLOR_BGRA2BGR)
-            cv2.imwrite(os.path.join(rgb_dir, f"{i:04d}.png"), color)
-            
-        depth = capture.depth  # uint16 mm
-        if depth is not None:
-            np.save(os.path.join(depth_dir, f"{i:04d}.npy"), depth)
-
-            # 可选：可视化depth
-            depth_vis = cv2.normalize(depth, None, 0, 255, cv2.NORM_MINMAX)
-            depth_vis = depth_vis.astype(np.uint8)
-            cv2.imwrite(os.path.join(depth_dir, f"{i:04d}.png"), depth_vis)
-
-        i += 1
-
-except EOFError:
-    print("✔ Finished reading MKV")
-
-finally:
-    playback.close()
+playback.close()
